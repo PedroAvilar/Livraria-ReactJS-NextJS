29d9255642c7f1bfd13715ce1b1091f93d858cf8
Pedro Avilar <pedrowa.avilar@gmail.com>
2024-05-13 14:40:58 -0300

    Criação do menu e rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LivroLista from './LivroLista';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import LivroDados from './LivroDados';
 
 function App() {
   return (
-    <div className='App'>
-      <LivroLista/>
-    </div>
+    <BrowserRouter>
+      <nav className='navbar navbar-expand navbar-dark bg-dark'>
+        <div className='container-fluid d-flex flex-row'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <Link to={'/'} className='nav-link'>Catálogo</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to={'/dados'} className='nav-link'>Novo</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path='/' element = {<LivroLista/>}/>
+        <Route path='/dados' element = {<LivroDados/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
