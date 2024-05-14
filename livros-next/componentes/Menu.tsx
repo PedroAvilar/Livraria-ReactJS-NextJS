1fd3bbc264272f142f9ad06c0ca74507fd7f8eab
Pedro Avilar <pedrowa.avilar@gmail.com>
2024-05-14 15:02:13 -0300

    Inclusão e formatação do menu.

--- livros-next/componentes/Menu.tsx
+++ livros-next/componentes/Menu.tsx
@@ -1,1 +1,28 @@
+import Link from "next/link";
 import MyApp from "./_app";
+
+export const Menu: React.FC = () => {
+    return(
+        <nav className='navbar navbar-expand navbar-dark bg-dark'>
+            <div className='container-fluid d-flex flex-row'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <Link href='/'>
+                            <a className="nav-link">Home</a>
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link href='/LivroLista'>
+                            <a className="nav-link">Catálogo</a>
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link href='/LivroDados'>
+                            <a className="nav-link">Novo</a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
